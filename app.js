a7c801f694982aa851e54d13f489d887ee9a6fbf
Ali-Adel-Nour <aliiiadel841@gmail.com>
2023-12-25 10:54:49 +0200

    finshing the frontend

---
+++ app.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const msg = new SpeechSynthesisUtterance();
+let voices = [];
+
+const voicesDropdown = document.querySelector("[name=voice]");
+const options = document.querySelectorAll("[type=range], [name=text]");
+const speakBtn = document.querySelector("#speak");
+const stopBtn = document.querySelector("#stop");
+
+msg.text = document.querySelector("[name=text]").value;
+
+// Push all available voices into the voices array
+function populateVoices() {
+  voices = this.getVoices();
+
+  const voicesOption = voices
+    .map((voice) => {
+      return `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`;
+    })
+    .join("");
+  voicesDropdown.innerHTML = voicesOption;
+}
+
+// find a voice that's equal to the voice selected and change to it
+function setVoice() {
+  msg.voice = voices.find((voice) => voice.name === this.value);
+  toggle(); //call the function for when a new voice is selected
+}
+
+// toggle function to either stop the ongoing speech and/or start a new one
+function toggle(startOver = true){
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+};
+
+// Set the pitch, rate and texts to alter to the changed value
+function setOption() {
+  msg[this.name] = this.value;
+  toggle();
+}
+
+speechSynthesis.addEventListener("voiceschanged", populateVoices);
+voicesDropdown.addEventListener("change", setVoice); //listen for a change in the selected voice
+
+options.forEach((option) => option.addEventListener("change", setOption));
+
+speakBtn.addEventListener("click", toggle);
+stopBtn.addEventListener("click", () => toggle(false))
+
+
+
